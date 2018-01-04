6590532b0005496f35258161cfa2fd68c7e58c68
Hecatier <flandle.lover@gmail.com>
2018-01-04 21:24:32 +0900

    ABC076 A問題

--- main.cpp
+++ main.cpp
@@ -12,35 +12,10 @@
 using namespace std;
 using ll = long long;
 
-bool isPrime(int x){
-    if(x < 2){
-        return false;
-    }
-    for(int i = 2; i <= sqrt(x); i++){
-        if(x%i == 0){
-            return false;
-        }
-    }
-    return true;
-}
-
-void solve(){
-    int a[100001];
-    int q;
-    cin >> q;
-    int l[q], r[q];
-
-    for(int i = 2; i <= 100000; i++){
-        if(isPrime(i) && isPrime((i+1)/2)){
-            a[i] = a[i-1]+1;
-        }else{
-            a[i] = a[i-1];
-        }
-    }
-    for(int i = 0; i < q; i++){
-        cin >> l[i] >> r[i];
-        cout << a[r[i]]-a[l[i]-1] << endl;
-    }
+void solve() {
+    int r, g;
+    cin >> r >> g;
+    cout << 2*g-r << endl;
 }
 
 int main(){
