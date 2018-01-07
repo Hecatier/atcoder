f14b74f3027dc9ead2d97bb7f4debbd5c880d4fd
Hecatier <flandle.lover@gmail.com>
2018-01-07 14:08:19 +0900

    ABC077 C問題

--- main.cpp
+++ main.cpp
@@ -8,38 +8,28 @@
 #include <climits>
 
 #define mod 1000000007
+#define REP(i,n) for(int i=0; i<n; i++)
 
 using namespace std;
 using ll = long long;
 
 void solve() {
-    int n, res = INT_MIN, c = 0, cc = 0;
+    int n, x;
+    ll res = 0;
+    vector<int> a, b, c;
     cin >> n;
-    int f[n][10], p[n][11];
+    REP(i, n) {cin >> x; a.push_back(x);}
+    REP(i, n) {cin >> x; b.push_back(x);}
+    REP(i, n) {cin >> x; c.push_back(x);}
 
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < 10; j++){
-            cin >> f[i][j];
-        }
-    }
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j <= 10; j++){
-            cin >> p[i][j];
-        }
-    }
+    sort(a.begin(), a.end());
+    sort(c.begin(), c.end());
 
-    for(int i = 1; i < (1<<10); i++){
-        cc = 0;
-        for(int j = 0; j < n; j++){
-            c = 0;
-            for(int k = 0; k < 10; k++){
-                if(((i>>k) & 1) && f[j][k]){ //1~1023を右シフトすると1のビットがいい感じにでてくる．それと&をとって全探索
-                    c++;    //他の店と同じ曜日がいくつあるか
-                }
-            }
-            cc += p[j][c];  //店の利益
-        }
-        res = max(res, cc);
+    unsigned long up, down;
+    REP(i, n){
+        up = (lower_bound(a.begin(), a.end(), b.at(i))-a.begin());
+        down = n-(upper_bound(c.begin(), c.end(), b.at(i))-c.begin());
+        res += up * down;
     }
     cout << res << endl;
 }
