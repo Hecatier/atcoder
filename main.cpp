b8b8f16b92d9c96f4b626b8af9f8338ceb789395
Hecatier <nishi.masahiro.17@shizuoka.ac.jp>
2018-01-10 18:14:12 +0900

    ABC074 A問題

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 #include <climits>
 
 #define mod 1000000007
-#define REP(i,n) for(int i=0; i<n; i++)
+#define REP(i,n) for(int (i)=0; (i)<(n); (i)++)
 
 using namespace std;
 using ll = long long;
@@ -16,37 +16,10 @@ using ll = long long;
 int X[] = {-1, 0, 1, 1, 1, 0, -1, -1};
 int Y[] = {1, 1, 1, 0, -1, -1, -1, 0};
 
-int h, w;
-char s[55][55];
-
-int neighborSearch(int x, int y){
-    int c = 0;
-    for(int i = 0; i < 8; i++){
-        if(s[x+X[i]][y+Y[i]] == '#'){
-            c++;
-        }
-    }
-    return c;
-}
-
 void solve() {
-    cin >> h >> w;
-    for(int i = 1; i < h+1; i++){
-        for(int j = 1; j < w+1; j++){
-            cin >> s[i][j];
-        }
-    }
-
-    for(int i = 1; i < h+1; i++){
-        for(int j = 1; j < w+1; j++){
-            if(s[i][j] == '.'){
-                cout << neighborSearch(i, j);
-            }else{
-                cout << s[i][j];
-            }
-        }
-        cout << endl;
-    }
+    int n, a;
+    cin >> n >> a;
+    cout << pow(n,2)-a << endl;
 }
 
 int main(){
