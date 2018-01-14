9f51eb0b487cc95fcff6546a363079bc0e31aa94
Hecatier <nishi.masahiro.17@shizuoka.ac.jp>
2018-01-14 21:57:29 +0900

    AGC20 A問題

--- main.cpp
+++ main.cpp
@@ -17,15 +17,22 @@ int X[] = {-1, 0, 1, 1, 1, 0, -1, -1};
 int Y[] = {1, 1, 1, 0, -1, -1, -1, 0};
 
 void solve() {
-    ll q, h, s, d, n;
-    cin >> q >> h >> s >> d >> n;
-    ll x = min(q*2, h);
-    ll y = min(x*2, s);
-    if(y*2 <= d){
-        cout << y*n << endl;
-    }else{
-        unsigned long long res = ((n/2)*d)+(n%2)*y;
-        cout << res << endl;
+    int n, a, b;
+    cin >> n >> a >> b;
+ 
+    while(1){
+        if(a < b-1){
+            a++;
+        }else{
+            cout << "Borys" << endl;
+            return;
+        }
+        if(b > a+1){
+            b--;
+        }else{
+          cout << "Alice" << endl;
+          return;
+        }
     }
 }
 
