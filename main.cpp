daaaf5c1d0580234d11ceabd0916893b22a43159
Hecatier <nishi.masahiro.17@shizuoka.ac.jp>
2018-02-08 12:58:54 +0900

    ABC007 A

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
 #define mod 1000000007
 #define REP(i,n) for(int (i)=0; (i)<(n); (i)++)
 #define FOREACH(i, v) for(auto i = v.begin(); i != v.end(); i++)
+#define PRINT(n) cout << n << endl
 
 using namespace std;
 using ll = long long;
@@ -22,9 +23,9 @@ int X[] = {-1, 0, 1, 1, 1, 0, -1, -1};
 int Y[] = {1, 1, 1, 0, -1, -1, -1, 0};
 
 void solve() {
-	int s, t;
-	cin >> s >> t;
-	cout << t-s+1 << endl;
+    int n;
+    cin >> n;
+    PRINT(n-1);
 }
 
 int main(){
