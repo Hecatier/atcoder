8fc5cac707b66f755ef7e5ba52b0078b13a620f3
Hecatier <nishi.masahiro.17@shizuoka.ac.jp>
2018-02-19 00:10:29 +0900

    ABC088 D

--- main.cpp
+++ main.cpp
@@ -8,95 +8,52 @@
 #include <climits>
 #include <set>
 #include <deque>
+#include <queue>
 #include <bitset>
-//#include "mylib.h"
 
 #define mod 1000000007
 #define REP(i,n) for(int (i)=0; (i)<(n); (i)++)
 #define FOREACH(i, v) for(auto i = v.begin(); i != v.end(); i++)
 #define PRINT(n) cout << n << endl
-//#define DEBUG
+#define P pair<int, int>
 
 using namespace std;
 using ll = long long;
 
-int dx[] = {-1, 0, 1, 1, 1, 0, -1, -1};
-int dy[] = {1, 1, 1, 0, -1, -1, -1, 0};
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
+int DX[] = {-1, 0, 1, 1, 1, 0, -1, -1};
+int DY[] = {1, 1, 1, 0, -1, -1, -1, 0};
+
+int h, w, c = 0, cc = 1;
+char s[55][55];
+
+int bfs(int sx, int sy, int gx, int gy, int N, int M){
+    int distance[h][w];
+    queue<P> que;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            distance[i][j] = -1;
+        }
+    }
+    que.push(P(sx, sy));
+    distance[sx][sy] = 0;
+
+    while(que.size()){
+        P p = que.front(); que.pop();
+        if(p.first == gx && p.second == gy) break;  //ゴールなら探索終了
+        for(int i = 0; i < 4; i++){
+            int nx = p.first + dx[i], ny = p.second + dy[i];    //nx, nyは移動後の点
+            if(0 <= nx && nx < N && 0 <= ny && ny < M && s[nx][ny] != '#' && distance[nx][ny] == -1){
+                que.push(P(nx, ny));
+                distance[nx][ny] = distance[p.first][p.second] + 1;
+            }
+        }
+    }
+    return distance[gx][gy];
+}
 
 void solve(){
-    int n = 100;
-    int a[n][n];
-    int Q;
-    vector<int> X, Y, H;
-
-    int x, y, v, z;
-    int Min = 100, MinX = 0, MinY = 0;
-
-    //input
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            cin >> a[i][j];
-        }
-    }
-
-    //algorithm
-    for(Q = 0; Q < 1000; Q++){
-        x = 0, y = 0, v = 0, z = 0;
-        for(int i = 1; i < n-1; i++){
-            for(int j = 1; j < n-1; j++){
-                if(a[i][j] < Min && a[i][j] > 0){
-                    Min = a[i][j];
-                    MinX = j;
-                    MinY = i;
-                }
-                int temp = a[i][j];
-                for(int k = 0; k < 8; k++){
-                    temp += a[i+dx[k]][j+dy[k]];
-                }
-                if(z < temp && temp > 2000){
-                    x = j;  //山のx座標
-                    y = i;  //山のy座標
-                    int distance = abs(MinX-x)+abs(MinY-y);
-                    if(distance > 100) distance = 100;
-                    v = max(Min, distance);   //山の値
-                    v = min(v, a[i][j]);
-                    z = temp;
-                }
-            }
-        }
-
-        //calc
-        if(v > 0){
-            X.push_back(x);
-            Y.push_back(y);
-            H.push_back(v);
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < n; j++){
-                    a[i][j] -= max(0, v-abs(x-j)-abs(y-i));
-                }
-            }
-        }else{
-            break;
-        }
-    }
-
-    //output
-    cout << Q << endl;
-    for(int i = 0; i < X.size(); i++){
-        cout << X[i] << " " << Y[i] << " " << H[i] << endl;
-    }
-
-#ifdef DEBUG
-    ll res = 0;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            cout << a[i][j] << " ";
-            res += abs(a[i][j]);
-        }
-        cout << endl;
-    }
-    clog << res << endl;
-#endif
 }
 
 int main(){
