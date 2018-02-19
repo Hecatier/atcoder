884776278bb0e60f9cd420b5ed693c5c70bc9543
Hecatier <nishi.masahiro.17@shizuoka.ac.jp>
2018-02-19 10:43:41 +0900

    研究室の環境に昨日の変更をマージ

--- main.cpp
+++ main.cpp
@@ -53,26 +53,7 @@ int bfs(int sx, int sy, int gx, int gy, int N, int M){
     return distance[gx][gy];
 }
 
-void solve(){
-    int a[3], b[3], c[3][3];
-    REP(i, 3) REP(j, 3) cin >> c[i][j];
-
-    a[0] = min(c[0][0], min(c[0][1], c[0][2]));
-    b[0] = c[0][0] - a[0];
-    a[1] = c[1][0] - b[0];
-    a[2] = c[2][0] - b[0];
-    b[1] = c[0][1] - a[0];
-    b[2] = c[0][2] - a[0]; 
-
-    REP(i, 3){
-        REP(j, 3){
-            if(c[i][j] != a[i]+b[j]){
-                cout << "No" << endl;
-                return;
-            }
-        }
-    }
-    cout << "Yes" << endl;
+void solve() {
 }
 
 int main(){
