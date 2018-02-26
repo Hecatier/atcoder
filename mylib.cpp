c8e7e520cd2eec7106eac419769ac7d17fead2e0
Hecatier <nishi.masahiro.17@shizuoka.ac.jp>
2018-02-26 14:18:09 +0900

    ライブラリにクラスカル法追加

--- mylib.cpp
+++ mylib.cpp
@@ -108,43 +108,70 @@ bool isNegativeCycle(){
     }
     return false;
 }
+//最小全域木(クラスカル法)
+struct UnionFind{
+    vector<int> par;
+    vector<int> sizes;
 
+    UnionFind(int n) : par(n), sizes(n, 1){
+        rep(i, n) par[i] = i;
+    }
 
-//union-find
-/*
-int par[MAX_N];
-int rank[MAX_N];
+    int find(int x){
+        if(x == par[x]) return x;
+        return par[x] = find(par[x]);
+    }
 
-void init(int n){
-	for(int i = 0; i < n; i++){
-		par[i] = i;
-		rank[i] = 0;
-	}
+    void unite(int x, int y){
+        x = find(x);
+        y = find(y);
+        if(x == y) return;
+        if(sizes[x] < sizes[y]) swap(x, y);
+        par[y] = x;
+        sizes[x] += sizes[y];
+    }
+
+    bool same(int x, int y){
+        return find(x) == find(y);
+    }
+
+    int size(int x){
+        return sizes[find(x)];
+    }
+};
+struct Edge{
+    int a, b, cost;
+    bool operator<(const Edge& o) const{
+        return cost < o.cost;
+    }
+};
+struct Graph{
+    int n;  //頂点数
+    vector<Edge> es;
+
+    int kruskal(){
+        sort(es.begin(), es.end());
+        UnionFind uf(n);
+        int min_cost = 0;
+
+        rep(i, es.size()){
+            Edge& e = es[i];
+            if(!uf.same(e.a, e.b)){
+                min_cost += e.cost;
+                uf.unite(e.a, e.b);
+            }
+        }
+        return min_cost;
+    }
+};
+Graph input_graph(){
+    Graph g;
+    int m;
+    cin >> g.n >> m;
+    rep(i, m){
+        Edge e;
+        cin >> e.a >> e.b >> e.cost;
+        g.es.push_back(e);
+    }
+    return g;
 }
-
-int find(int x){
-	if(par[x] == x){
-		return x;
-	}else{
-		return par[x] = find(par[x]);
-	}
-}
-
-void unite(int x, int y){
-	x = find(x);
-	y = find(y);
-
-	if(x == y) return;
-
-	if(rank[x] < rank[y]){
-		par[x] = y;
-	}else{
-		par[y] = x;
-		if(rank[x] == rank[y]) rank[x]++;
-	}
-}
-
-bool same(int x, int y){
-	return find(x) == find(y);
-}
-*/
